bd8a5df0623a78ea64aae6700ebb3e1afe168b57
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-10 18:33:40 +0200

    exercise 1: starting point

---
+++ README.md
@@ -0,0 +1,3 @@
+# TDD
+
+This repo contains my submissions to the TDD course at the University of Helsinki, winter 2023.
---
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -0,0 +1,101 @@
+import "./polyfills.mjs";
+import express from "express";
+
+// Refactor the following code to get rid of the legacy Date class.
+// Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
+
+function createApp(database) {
+  const app = express();
+
+  app.put("/prices", (req, res) => {
+    const liftPassCost = req.query.cost;
+    const liftPassType = req.query.type;
+    database.setBasePrice(liftPassType, liftPassCost);
+    res.json();
+  });
+
+  app.get("/prices", (req, res) => {
+    const age = req.query.age;
+    const type = req.query.type;
+    const baseCost = database.findBasePriceByType(type).cost;
+    const date = parseDate(req.query.date);
+    const cost = calculateCost(age, type, date, baseCost);
+    res.json({ cost });
+  });
+
+  function parseDate(dateString) {
+    if (dateString) {
+      return new Date(dateString);
+    }
+  }
+
+  function calculateCost(age, type, date, baseCost) {
+    if (type === "night") {
+      return calculateCostForNightTicket(age, baseCost);
+    } else {
+      return calculateCostForDayTicket(age, date, baseCost);
+    }
+  }
+
+  function calculateCostForNightTicket(age, baseCost) {
+    if (age === undefined) {
+      return 0;
+    }
+    if (age < 6) {
+      return 0;
+    }
+    if (age > 64) {
+      return Math.ceil(baseCost * 0.4);
+    }
+    return baseCost;
+  }
+
+  function calculateCostForDayTicket(age, date, baseCost) {
+    let reduction = calculateReduction(date);
+    if (age === undefined) {
+      return Math.ceil(baseCost * (1 - reduction / 100));
+    }
+    if (age < 6) {
+      return 0;
+    }
+    if (age < 15) {
+      return Math.ceil(baseCost * 0.7);
+    }
+    if (age > 64) {
+      return Math.ceil(baseCost * 0.75 * (1 - reduction / 100));
+    }
+    return Math.ceil(baseCost * (1 - reduction / 100));
+  }
+
+  function calculateReduction(date) {
+    let reduction = 0;
+    if (date && isMonday(date) && !isHoliday(date)) {
+      reduction = 35;
+    }
+    return reduction;
+  }
+
+  function isMonday(date) {
+    return date.getDay() === 1;
+  }
+
+  function isHoliday(date) {
+    const holidays = database.getHolidays();
+    for (let row of holidays) {
+      let holiday = new Date(row.holiday);
+      if (
+        date &&
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  return app;
+}
+
+export { createApp };
