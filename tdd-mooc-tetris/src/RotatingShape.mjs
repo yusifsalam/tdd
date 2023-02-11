291364cac1791084048c7694af427ec70032996d
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-11 21:14:59 +0200

    generic algoright to rotate clockwise

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -6,18 +6,9 @@ export class RotatingShape {
   }
 
   rotateRight() {
-    const clone = structuredClone(this.grid);
-    this.grid[0][0] = clone[2][0];
-    this.grid[0][1] = clone[1][0];
-    this.grid[0][2] = clone[0][0];
-
-    this.grid[1][0] = clone[2][1];
-    this.grid[1][1] = clone[1][1];
-    this.grid[1][2] = clone[0][1];
-
-    this.grid[2][0] = clone[2][2];
-    this.grid[2][1] = clone[1][2];
-    this.grid[2][2] = clone[0][2];
+    this.grid = this.grid.map((val, i) =>
+      this.grid.map((row) => row[i]).reverse()
+    );
     return this.toString();
   }
 
