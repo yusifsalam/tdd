d72ffd98eea750ff221a78f300d5bb2e4f55a69b
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-11 21:09:08 +0200

    3x3 block can be rotated clockwise

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -5,6 +5,22 @@ export class RotatingShape {
     this.grid = value.split("\n").map((a) => a.trim().split(""));
   }
 
+  rotateRight() {
+    const clone = structuredClone(this.grid);
+    this.grid[0][0] = clone[2][0];
+    this.grid[0][1] = clone[1][0];
+    this.grid[0][2] = clone[0][0];
+
+    this.grid[1][0] = clone[2][1];
+    this.grid[1][1] = clone[1][1];
+    this.grid[1][2] = clone[0][1];
+
+    this.grid[2][0] = clone[2][2];
+    this.grid[2][1] = clone[1][2];
+    this.grid[2][2] = clone[0][2];
+    return this.toString();
+  }
+
   toString() {
     return this.grid.map((row) => row.join("") + "\n").join("");
   }
