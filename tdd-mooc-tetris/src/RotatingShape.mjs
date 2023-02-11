21ff28cd60edfd7b74a1e9a21bfb166258374b93
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-11 21:25:33 +0200

    refactor: replace unused param with underscore

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -6,14 +6,14 @@ export class RotatingShape {
   }
 
   rotateRight() {
-    const rotatedRight = this.grid.map((val, i) =>
+    const rotatedRight = this.grid.map((_, i) =>
       this.grid.map((row) => row[i]).reverse()
     );
     return rotatedRight.map((row) => row.join("") + "\n").join("");
   }
 
   rotateLeft() {
-    const rotatedLeft = this.grid.map((val, i) =>
+    const rotatedLeft = this.grid.map((_, i) =>
       this.grid.map((row) => row[row.length - 1 - i])
     );
     return rotatedLeft.map((row) => row.join("") + "\n").join("");
