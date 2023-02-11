c517fb93e0d71d124a19ac50f4587d96d20d2e60
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-11 21:23:12 +0200

    3x3 shape can be rotated left

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -12,6 +12,20 @@ export class RotatingShape {
     return rotatedRight.map((row) => row.join("") + "\n").join("");
   }
 
+  rotateLeft() {
+    const clone = structuredClone(this.grid);
+    clone[0][0] = this.grid[0][2];
+    clone[0][1] = this.grid[1][2];
+    clone[0][2] = this.grid[2][2];
+    clone[1][0] = this.grid[0][1];
+    clone[1][1] = this.grid[1][1];
+    clone[1][2] = this.grid[2][1];
+    clone[2][0] = this.grid[0][0];
+    clone[2][1] = this.grid[1][0];
+    clone[2][2] = this.grid[2][0];
+    return clone.map((row) => row.join("") + "\n").join("");
+  }
+
   toString() {
     return this.grid.map((row) => row.join("") + "\n").join("");
   }
