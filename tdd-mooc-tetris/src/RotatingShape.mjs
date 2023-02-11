b5703ae2de1e70ceac22d92d83f8458375332aed
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-11 20:53:15 +0200

    refactor: trim inside the constructor

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -2,14 +2,14 @@ export class RotatingShape {
   value;
 
   constructor(value) {
-    this.value = value;
-  }
-
-  toString() {
-    return this.value
+    this.value = value
       .split("\n")
       .map((a) => a.trim())
       .join("\n")
       .concat("\n");
+  }
+
+  toString() {
+    return this.value.toString();
   }
 }
