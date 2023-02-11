96240826a5e7bd805ed8806a33a5c55e793ec703
Yusif Salam-zade <42574232+yusifsalam@users.noreply.github.com>
2023-02-11 22:30:48 +0200

    fix: return new RotatingShape when rotating

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -1,25 +1,27 @@
 export class RotatingShape {
-  grid;
+  #grid;
 
   constructor(value) {
-    this.grid = value.split("\n").map((a) => a.trim().split(""));
+    this.#grid = value.split("\n").map((a) => a.trim().split(""));
   }
 
   rotateRight() {
-    const rotatedRight = this.grid.map((_, i) =>
-      this.grid.map((row) => row[i]).reverse()
-    );
-    return rotatedRight.map((row) => row.join("") + "\n").join("");
+    const rotatedRight = this.#grid
+      .map((_, i) => this.#grid.map((row) => row[i]).reverse())
+      .map((row) => row.join("") + "\n")
+      .join("");
+    return new RotatingShape(rotatedRight.slice(0, rotatedRight.length - 1));
   }
 
   rotateLeft() {
-    const rotatedLeft = this.grid.map((_, i) =>
-      this.grid.map((row) => row[row.length - 1 - i])
-    );
-    return rotatedLeft.map((row) => row.join("") + "\n").join("");
+    const rotatedLeft = this.#grid
+      .map((_, i) => this.#grid.map((row) => row[row.length - 1 - i]))
+      .map((row) => row.join("") + "\n")
+      .join("");
+    return new RotatingShape(rotatedLeft.slice(0, rotatedLeft.length - 1));
   }
 
   toString() {
-    return this.grid.map((row) => row.join("") + "\n").join("");
+    return this.#grid.map((row) => row.join("") + "\n").join("");
   }
 }
